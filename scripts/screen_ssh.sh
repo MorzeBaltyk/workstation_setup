df72216d2ea5d3ccaf6db62e0ff4ccbde68231b3
MorzeBaltyk <chrisdelart@hotmail.fr>
2019-11-07 18:29:47 +0100

    Screen_ssh adadpted

--- scripts/screen_ssh.sh
+++ scripts/screen_ssh.sh
@@ -3,10 +3,11 @@ key='r'
 while [[ $key == 'r' ]]
 do
 
-	if $(echo $1 | grep -vq '\-sc')
-	then
-	        scp -q ~/bash_profile.tmp ~/.privIncludes/*.bash.tmp root@$1:/tmp/ 2>/dev/null
-	fi
+# Commented because of Homedir shared through NFS.
+#	if $(echo $1 | grep -vq '\-sc')
+#	then
+#	        scp -q ~/bash_profile.tmp ~/.privIncludes/*.bash.tmp root@$1:/tmp/ 2>/dev/null
+#	fi
 
 
 	/usr/bin/ssh $* || echo ''; read -n1 -r -p "Press 'r' to restart connection or any other key to close this screen tab: " key
