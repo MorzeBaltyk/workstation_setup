4dfe8eb5d457f0741d0ef5d09b06313ec5fb131d
MorzeBaltyk <chrisdelart@hotmail.fr>
2019-11-25 14:39:21 +0100

    Get Host Record

--- .bashIncludes/A_get_functions.bash
+++ .bashIncludes/A_get_functions.bash
@@ -8,15 +8,27 @@ function getOS ()
 
 function get_host_records ()
 {
-    check_input $@ || return 1;
-    OneHOST=`echo "$1" | tr '[:upper:]' '[:lower:]'`;
+    check_input $@ || return 1
+    no_more_than_one $@ || return 1
+    H=$1
+    DOMAIN=.opoce.cec.eu.int
+    HOST=${H}${DOMAIN}
+    ADDR=`dig ${HOST} +short | sed 's/\.$//'`
+    if [ -n "$ADDR" ]; then
+        if [ `dig $HOST | grep "^$HOST" | awk '{print $4}'` == "A" ]; then
+            REVERSE=`dig -x $ADDR | awk '/'$HOST'/{print $1}'| sed 's/\.$//'`
+            printf "%-40s %-12s %s\n" $HOST "A" $ADDR $REVERSE "PTR" $HOST
+        else
+            if [ `dig $HOST | grep "^$HOST" | awk '{print $4}'` == "CNAME" ]; then
+                printf "%-40s %-12s %-40s %s\n" $HOST "CNAME" $ADDR
+            fi
+        fi
+    fi
 
 }
 
-function test () 
+function get_host_records_list () 
 {
-myOS=$(getOS $1)
-echo $?
-#if [[ "$(getOS $1)" != 0 ]]; then msg_ok "OK"; else msg_error "NOK"; fi
+	BKP=""
+	for h in $@; do get_host_records $h; done
 }
-
