dd94e25c394d8d747653c3a2f77b0fb40eddd27a
MorzeBaltyk <chrisdelart@hotmail.fr>
2019-11-28 18:34:30 +0100

    correct in ldap_function

--- bashIncludes/ldap_functions.bash
+++ bashIncludes/ldap_functions.bash
@@ -9,48 +9,48 @@ function ldapsearchexists ()
   return 1
   fi
 #else
-#  errmsg "Not Solaris, exiting ..."
+#  msg_error "Not Solaris, exiting ..."
 #  return 1
 #fi
 }
 
 function ldapsearchaliases ()
 {
-[ "$#" -eq 0 ] && echo "Usage: $FUNCTNAME <userid>" && return 1
+[ "$#" -eq 0 ] && msg_help "$FUNCNAME <userid>" && return 1
 ldapsearchexists || return 1
 ldapsearch -x 2> /dev/null -h $LDAPSERVER -b 'ou=Aliases,dc=opoce,dc=cec,dc=eu,dc=int' cn=$1 | grep -v ^version
 }
 
 function ldapsearchemail ()
 {
-[ "$#" -eq 0 ] && echo "Usage: $FUNCTNAME <userid>" && return 1
+[ "$#" -eq 0 ] && msg_help "$FUNCNAME <userid>" && return 1
 ldapsearchexists || return 1
 ldapsearch -x 2> /dev/null -h $LDAPSERVER -b 'ou=Aliases,dc=opoce,dc=cec,dc=eu,dc=int' cn=$1 rfc822MailMember | grep rfc822MailMember | awk '{print $2}'
 }
 
 function ldapsearchuser ()
 {
-[ "$#" -eq 0 ] && echo "Usage: $FUNCTNAME <userid>" && return 1
+[ "$#" -eq 0 ] && msg_help "$FUNCNAME <userid>" && return 1
 ldapsearchexists || return 1
 ldapsearch -x 2> /dev/null -h $LDAPSERVER -b 'ou=people,dc=opoce,dc=cec,dc=eu,dc=int' uid=$1 | grep -v ^version
 }
 
 function ldapsearchgecos ()
 {
-[ "$#" -eq 0 ] && echo "Usage: $FUNCTNAME <userid>" && return 1
+[ "$#" -eq 0 ] && msg_help "$FUNCNAME <userid>" && return 1
 ldapsearchexists || return 1
 ldapsearch -x 2> /dev/null -h $LDAPSERVER -b 'ou=people,dc=opoce,dc=cec,dc=eu,dc=int' uid=$1 gecos | awk -F":" '/gecos/ {print $NF}'| xargs
 }
 
 function ldapsearchusergidnumber ()
 {
-[ "$#" -eq 0 ] && echo "Usage: $FUNCTNAME <userid>" && return 1
+[ "$#" -eq 0 ] && msg_help "$FUNCNAME <userid>" && return 1
 ldapsearchexists || return 1
 ldapsearch -x 2> /dev/null -h $LDAPSERVER -b 'ou=people,dc=opoce,dc=cec,dc=eu,dc=int' uid=claeyje gidNumber | awk '/gidNumber/ {print $NF}'
 }
 function ldapsearchsolarisroles ()
 {
-[ "$#" -eq 0 ] && echo "Usage: $FUNCTNAME <userid>" && return 1
+[ "$#" -eq 0 ] && msg_help "$FUNCNAME <userid>" && return 1
 ldapsearchexists || return 1
 ldapsearch -x 2> /dev/null -h $LDAPSERVER -b 'ou=people,dc=opoce,dc=cec,dc=eu,dc=int' uid=$1 | grep SolarisAttrKeyValue
 }
@@ -88,7 +88,7 @@ ldapsearch -x -h $LDAPSERVER -b 'ou=people,dc=opoce,dc=cec,dc=eu,dc=int' -s sub 
 function ldapsearchautomount ()
 {
 ldapsearchexists || return 1
-[ $# -eq 0 ] && msg_help "Usage: $FUNCNAME <uid>" && return 1
+[ $# -eq 0 ] && msg_help "$FUNCNAME <uid>" && return 1
 ldapsearch -x -o ldif-wrap=no -h $LDAPSERVER -b 'dc=opoce,dc=cec,dc=eu,dc=int' -s sub "(&(objectclass=automount)(automountkey=$1))"
 }
 
@@ -102,7 +102,7 @@ function ldapsearchusergroups ()
 {
 local F
 ldapsearchexists || return 1
-[ $# -eq 0 ] && msg_help "Usage: $FUNCNAME <uid>" && return 1
+[ $# -eq 0 ] && msg_help "$FUNCNAME <uid>" && return 1
 ldapsearch -x 2> /dev/null -h $LDAPSERVER -b 'ou=group,dc=opoce,dc=cec,dc=eu,dc=int' memberuid=$1| awk '/cn:/ {print $NF}' | sort | xargs
 }
 
@@ -110,7 +110,7 @@ function ldapsearchusergroupsfull ()
 {
 local F
 ldapsearchexists || return 1
-[ $# -eq 0 ] && msg_help "Usage: $FUNCNAME <uid>" && return 1
+[ $# -eq 0 ] && msg_help "$FUNCNAME <uid>" && return 1
 # ldapsearch -x 2> /dev/null -h $LDAPSERVER -b 'ou=group,dc=opoce,dc=cec,dc=eu,dc=int' memberuid=$1| awk '/cn:/ {print $NF}' | sort | xargs
 F="uid=$1,ou=people,dc=opoce,dc=cec,dc=eu,dc=int"
 ldapsearch -x -h $LDAPSERVER -b 'ou=group,dc=opoce,dc=cec,dc=eu,dc=int' "(|(memberuid=$1)(uniquemember="$F")(member=$F))" cn | awk '/cn:/ {print $NF}' | sort | xargs
@@ -119,7 +119,7 @@ ldapsearch -x -h $LDAPSERVER -b 'ou=group,dc=opoce,dc=cec,dc=eu,dc=int' "(|(memb
 function ldapsearchopdt ()
 {
 ldapsearchexists || return 1
-[ $# -eq 0 ] && msg_help "Usage: $FUNCNAME <opdtxxx>" && return 1
+[ $# -eq 0 ] && msg_help "$FUNCNAME <opdtxxx>" && return 1
 #ldapsearch 2> /dev/null -h $LDAPSERVER -b 'ou=netgroup,dc=opoce,dc=cec,dc=eu,dc=int' cn=admin | grep -v ^version | egrep -i "cn:|dn:|objectclass|$1"
 ldapsearch -x 2> /dev/null -h $LDAPSERVER -b 'ou=netgroup,dc=opoce,dc=cec,dc=eu,dc=int' cn=* | grep -v ^version | egrep -i "cn:|dn:|objectclass|$1" | grep -B4 -i $1 | grep -iv objectclass
 }
