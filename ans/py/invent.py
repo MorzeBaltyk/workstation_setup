e09e26579a3e3514c4e288309f7c3c155bc12054
MorzeBaltyk <chrisdelart@hotmail.fr>
2019-12-03 17:22:26 +0100

    correct repo.yml

--- ans/py/invent.py
+++ ans/py/invent.py
@@ -2,51 +2,62 @@
 
 # ansible "getcmdb" inventory script
 
-import json
 import csv
+import json
 import urllib3
-import io
 import sys
+import re
 
-#hostLists = {
-#    "_meta": {
-#        "hostvars": {}
-#    },  # See https://docs.ansible.com/ansible/latest/dev_guide/developing_inventory.html#tuning-the-external-inventory-script
-#    "all": {
-#        "children": ["ungrouped", "solaris", "linux"]
-#    },  # The all group is just a supergroup for all hosts
-#    "linux": {
-#        "children": ["redhat5", "redhat6", "redhat7"]
-#    },  # A supergroup just for Linux hosts
-#    "restrictedNET": {"children": ["SHS", "DMZ"]},  # A supergroup for SHS and DMZ hosts
-#    "ungrouped": [],  # According to the doc, this should be a dict, but ansible thinks it's a host and throws a warning. As a list, it's fine.
-#}
+#################################################
+hostLists = {
+              "_meta"         : { "hostvars": {} }, # See https://docs.ansible.com/ansible/latest/dev_guide/developing_inventory.html#tuning-the-external-inventory-script
+              "all"           : { "children": ['ungrouped', 'solaris', 'linux'] }, # The all group is just a supergroup for all hosts
+              "linux"         : { "children": ['redhat6', 'redhat7'] }, # A supergroup just for Linux hosts
+              "ungrouped"     : [] # According to the doc, this should be a dict, but ansible thinks it's a host and throws a warning. As a list, it's fine.
+}
 
-#for group in ["solaris", "redhat5", "redhat6", "redhat7", "SHS", "DMZ", "LAN"]: hostLists[group] = []  # Initialize empty host lists for every host grouping we want.
+for group in ['solaris', 'redhat6', 'redhat7']: hostLists[group] = [] # Initialize empty host lists for every host grouping we want.
+################################################
 
 # Linux
 http = urllib3.PoolManager()
 r = http.request('GET', 'http://infra1-pk:8181/modules/mpirequester/lists.php?id=76')
+
+# reformat my input as CSV
 data = "".join(map(chr,r.data))
 data = data.split('\n')
-
-
+all_linux_csv = csv.DictReader(data, delimiter=';')
+#for row in all_linux_csv:
+#   print(row)
 
 # Print Brutto
 #print(r.data)
 
 # Print Netto
-#data = "".join(map(chr,r.data))
-#data = data.split('\n')
-#for row in data:
-#    print(row)
-
-
-
-#csv_read = csv.reader(r)
-#for line in csv_read:
-#  print(line)
+#if sys.argv[1] == '--debug':
+#    for row in all_linux_csv:
+#        print(row)
 
 # Solaris
 #r = http.request('GET', 'http://infra1-pk:8181/modules/mpirequester/lists.php?id=26')
-#r.data
+
+
+##########################################################
+for hostObj in all_linux_csv:
+  # if sys.argv[1] == '--debug': print(hostObj)
+  # v = re.search("*.el6.*", (hostObj['OS KERNEL PATCH']))
+  # if hostObj['OS KERNEL PATCH'] == v: print(hostObj['NAME']) #hostLists['redhat6'].append(hostObj['NAME']) 
+  if re.search("*.el6.*", (hostObj['OS KERNEL PATCH'])): print(hostObj['NAME'])
+
+
+#############################################################
+#   Arguments Manadatory for Ansible Compliance
+#############################################################
+#if sys.argv[1] == '--list': # Mandatory return for ansible. Should return a list of all hosts
+#   print json.dumps(hostLists)
+
+#if sys.argv[1] == '--host': # Mandatory return for ansible. Should return an empty dict or vars for one host (vars not implemented)
+#   print json.loads('{}')
+
+
+
