ffb766bc99e2bbfdb73a78a68782102c2727d4e0
MorzeBaltyk <chrisdelart@hotmail.fr>
2019-12-05 18:03:09 +0000

    patch j

--- ans/py/invent.py
+++ ans/py/invent.py
@@ -7,6 +7,21 @@ import json
 import urllib3
 import sys
 import re
+import os
+import subprocess
+import socket
+import argparse
+
+
+def parseArgs(): # Function which parses the command-line arguments we expect.
+   parser = argparse.ArgumentParser() # We uses the argparse library (https://docs.python.org/3/library/argparse.html)
+   parser.add_argument('--debug','-d', action='store_true', help='Show debug stuff')
+   parser.add_argument('--list','-l', action='store_true', help='Mandatory return for ansible. Should return a list of all hosts')
+   #parser.add_argument('--host','-o', type=str, help='Mandatory return for ansible. Should return an empty dict or vars for one host (vars not implemented)')
+   return parser.parse_args()
+
+if __name__ == "__main__":
+   args = parseArgs()
 
 #################################################
 hostLists = {
@@ -17,16 +32,17 @@ hostLists = {
 }
 
 for group in ['solaris', 'redhat6', 'redhat7']: hostLists[group] = [] # Initialize empty host lists for every host grouping we want.
-################################################
+
+##################################################
 
 # Linux
 http = urllib3.PoolManager()
 r = http.request('GET', 'http://infra1-pk:8181/modules/mpirequester/lists.php?id=76')
-
-# reformat my input as CSV
-data = "".join(map(chr,r.data))
+data = "".join(map(chr,r.data)) # reformat my input as CSV
 data = data.split('\n')
 all_linux_csv = csv.DictReader(data, delimiter=';')
+
+
 #for row in all_linux_csv:
 #   print(row)
 
@@ -44,17 +60,23 @@ all_linux_csv = csv.DictReader(data, delimiter=';')
 
 ##########################################################
 for hostObj in all_linux_csv:
-  # if sys.argv[1] == '--debug': print(hostObj)
-  # v = re.search("*.el6.*", (hostObj['OS KERNEL PATCH']))
-  # if hostObj['OS KERNEL PATCH'] == v: print(hostObj['NAME']) #hostLists['redhat6'].append(hostObj['NAME']) 
-  if re.search("*.el6.*", (hostObj['OS KERNEL PATCH'])): print(hostObj['NAME'])
+  if args.debug: 
+      for row in all_linux_csv: 
+          print(row)
+
+  if re.search(r".*el6.*", (hostObj['OS KERNEL PATCH'])): hostLists['redhat6'].append(hostObj['NAME']) #print(hostObj['NAME'])
+  if re.search(r".*el7.*", (hostObj['OS KERNEL PATCH'])): hostLists['redhat7'].append(hostObj['NAME']) #print(hostObj['NAME'])
 
 
 #############################################################
 #   Arguments Manadatory for Ansible Compliance
 #############################################################
-#if sys.argv[1] == '--list': # Mandatory return for ansible. Should return a list of all hosts
-#   print json.dumps(hostLists)
+if args.list:
+    print(json.dumps(hostLists))
+
+#if args.host:
+    #print(args.host)
+    #print(json.loads('{}')
 
 #if sys.argv[1] == '--host': # Mandatory return for ansible. Should return an empty dict or vars for one host (vars not implemented)
 #   print json.loads('{}')
